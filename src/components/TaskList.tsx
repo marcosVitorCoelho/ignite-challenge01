24ac286a3d5ad0a045b5537568728a4d821cff5c
marcosVitorCoelho <marcosvitorcoelho@gmail.com>
2021-09-22 00:28:53 -0300

    :tada: Challenge finished

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,35 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+
+    const newTask = {
+      id: Math.random() * Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks(oldState => [...oldState, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
+    const taskIndex = tasks.findIndex(tasks => tasks.id === id);
+
+    const taskCompletion = [...tasks];
+    
+    taskCompletion[taskIndex].isComplete = !taskCompletion[taskIndex].isComplete
+
+    setTasks(taskCompletion)
+    console.log(taskIndex); //confirmando que o index da task pelo id foi selecionado
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(filteredTasks)
   }
 
   return (
